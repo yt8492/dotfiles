e9cb552e74ca7c5a9ccec0cd059bc7654982c7a8
yt8492 <tmymyut2000@gmail.com>
2020-09-24 02:10:34 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
-brew install wget
 brew cask
+brew tap homebrew/cask-versions
 brew cask install java11
 brew cask install adoptopenjdk8
 brew install sbt
@@ -12,11 +12,9 @@ brew install docker
 brew cask install docker
 brew install anyenv
 anyenv install --init
-eval "$(anyenv init -)"
 anyenv install nodenv
-eval "$(nodenv init -)"
 anyenv install jenv
-eval "$(jenv init -)"
+eval "$(anyenv init -)"
 nodenv install 12.18.3
 nodenv global 12.18.3
 jenv add `/usr/libexec/java_home -v "1.8"`
@@ -24,10 +22,10 @@ jenv add `/usr/libexec/java_home -v "11"`
 jenv enable-plugin export
 chsh -s /bin/zsh
 npm install --global pure-prompt
-brew install zsh-syntax-highlighting
 brew cask install android-studio
 brew cask install intellij-idea
 brew cask install webstorm
 brew cask install goland
 brew cask install visual-studio-code
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 curl https://raw.githubusercontent.com/yt8492/dotfiles/master/.zshrc > ~/.zshrc
