522d76ae420b16aa0cd9c9cdc55d75841bfe9194
yt8492 <tmymyut2000@gmail.com>
2020-11-04 19:11:35 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -2,23 +2,20 @@
 brew update
 brew cask
 brew tap homebrew/cask-versions
-brew cask install java11
 brew cask install adoptopenjdk8
 brew install sbt
 brew install go
-brew install rbenv
-brew install ruby-build
 brew install docker
 brew cask install docker
 brew install anyenv
-anyenv install --init
+yes | anyenv install --init
 anyenv install nodenv
 anyenv install jenv
+anyenv install rbenv
 eval "$(anyenv init -)"
 nodenv install 12.18.3
 nodenv global 12.18.3
 jenv add `/usr/libexec/java_home -v "1.8"`
-jenv add `/usr/libexec/java_home -v "11"`
 jenv enable-plugin export
 chsh -s /bin/zsh
 npm install --global pure-prompt
